136c6a034d754e2af8d04d42528125c34efdfc87
Julien.Brunet <julien.brunet@cegeplimoilou.ca>
2023-09-04 20:12:41 -0400

    cours 4

--- src/semaine02/methodes/IntroMethodes.java
+++ src/semaine02/methodes/IntroMethodes.java
@@ -7,12 +7,37 @@ package semaine02.methodes;
  * @since 2023-08-29
  */
 public class IntroMethodes {
+
     public static void main(String[] args) {
 
-        new IntroMethodes();
+        IntroMethodes toto = new IntroMethodes();
 
+
+        toto.afficher3fois4();
+
+        int a= 12;
+        int b = 13;
+
+        toto.afficherMultiplication(a,b);
+
+        toto.afficherMultiplication(5,8);
+
+        toto.afficherMultiplication(3,0);
+
+        toto.afficherMultiplication(167,9876);
+
+        int res = toto.calculerMultiplication(10,5);
+        System.out.println("10 * 5 = " + res);
+    }
+    public void afficherMultiplication(int premierNombre,int deuxiemeNombre){
+        int resultat = premierNombre*deuxiemeNombre;
+        System.out.println(premierNombre + " * " +deuxiemeNombre + " = " + resultat);
     }
 
+    public int calculerMultiplication(int premierNombre,int deuxiemeNombre){
+        int resultat = premierNombre*deuxiemeNombre;
+        return resultat;
+    }
 
     public void afficher3fois4() {
 
@@ -22,11 +47,5 @@ public class IntroMethodes {
     }
 
 
-
-
-
-    public IntroMethodes(){
-        afficher3fois4();
-    }
 
 }

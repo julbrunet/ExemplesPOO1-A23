9d11308afc19aa7d23e7a9a5c12ea74e2df6f961
Julien.Brunet <julien.brunet@cegeplimoilou.ca>
2023-09-11 21:16:53 -0400

    cours 7

---
+++ src/semaine04/exemples/TestOperateurBooleans.java
@@ -0,0 +1,39 @@
+package semaine04.exemples;
+
+/**
+ * ExemplesPOO1
+ *
+ * @author Julien.Brunet
+ * @since 2023-09-11
+ */
+public class TestOperateurBooleans {
+    public static void main(String[] args) {
+        TestOperateurBooleans test = new
+                TestOperateurBooleans();
+        test.testeOperateurs();
+    }
+
+    private void testeOperateurs() {
+
+        System.out.println(testAnd(2,6));
+        System.out.println(testAnd(5,10));
+        System.out.println(testAnd(2,11));
+        System.out.println(testAnd(6,11));
+
+
+        System.out.println(testOr(2,6));
+        System.out.println(testOr(5,10));
+        System.out.println(testOr(2,11));
+        System.out.println(testOr(6,11));
+
+    }
+
+    public boolean testAnd(int a , int b){
+        return a < 5 && b<=10;
+    }
+
+    public boolean testOr(int a , int b){
+        return a < 5 || b<=10;
+    }
+
+}
